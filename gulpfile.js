e07cc1ce589e67d619c3582f6ed77d8e01f58fe1
Tim Brockman <tim@timbrockman.com>
2016-12-16 21:25:55 -0500

    formating

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,24 @@
 'use strict'
 //general
-var gulp = require( 'gulp');
-var gutil = require('gulp-util');
-var rename = require('gulp-rename');
-var clean = require('gulp-clean');
-var merge = require('merge-stream');
-var data = require('gulp-data');
+var gulp = require( 'gulp'),
+  gutil = require('gulp-util'),
+  rename = require('gulp-rename'),
+  clean = require('gulp-clean'),
+  merge = require('merge-stream'),
+  data = require('gulp-data');
 //templating
 var frontMatter = require('gulp-front-matter'),
-  marked = require('gulp-marked');
-var handlebars = require('gulp-compile-handlebars');
+  marked = require('gulp-marked'),
+  handlebars = require('gulp-compile-handlebars');
 //other build and deploy
-var images = require('gulp-responsive-images');
-var ghpages = require('gulp-gh-pages');
+var images = require('gulp-responsive-images'),
+  ghpages = require('gulp-gh-pages');
 //site info
 var site = require('./site.json');
 site.time = new Date();
 
 //templating
 gulp.task('grind-pages', ()=>{
-  
   return gulp.src('content/pages/*.md')
     .pipe(frontMatter({
       property:'page',
